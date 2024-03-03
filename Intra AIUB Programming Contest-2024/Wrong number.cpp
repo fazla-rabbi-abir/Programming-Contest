477e651386b638bb47d072417a204a44a11605b6
Md Fazla Rabbi Abir <131839058+fazla-rabbi-abir@users.noreply.github.com>
2024-03-03 10:37:15 +0600

    Add files via upload

---
+++ Intra AIUB Programming Contest-2024/Wrong number.cpp
@@ -0,0 +1,22 @@
+/*
+B. Wrong Answer
+Limits 1s, 512 MB
+Sometimes, you need to get a wrong answer before you can actually get an “Accepted“ verdict.
+
+Just “wrong answer“ to solve this problem.
+
+Input
+No input for this problem.
+
+Output
+wrong answer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "wrong answer";
+
+}
